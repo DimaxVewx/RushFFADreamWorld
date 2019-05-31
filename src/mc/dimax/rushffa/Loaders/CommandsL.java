3b68b03d77efa01e48c9e9caadfb97a623a40eb0
HichamBelhamiti <bativewx@gmail.com>
2019-05-31 20:14:52 +0200

    - Delete class in commands - delete methode in CommandsL ( Loader )

--- src/mc/dimax/rushffa/Loaders/CommandsL.java
+++ src/mc/dimax/rushffa/Loaders/CommandsL.java
@@ -1,11 +1,9 @@
 package mc.dimax.rushffa.Loaders;
 
-import mc.dimax.rushffa.Commands.CommandeSetSpawn;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandsL {
     public static void load(JavaPlugin plugin){
-        plugin.getCommand("setspawn").setExecutor(new CommandeSetSpawn());
 
     }
 }
